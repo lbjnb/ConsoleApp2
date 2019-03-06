7e9563e73e16b8880f4c73718e3f92790e61dc4d
ziji <ziji@DESKTOP-QSO21IB>
2019-03-06 21:29:55 +0800

    添加项目文件。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Hello
+    {
+        public Hello(String[] args)
+        {
+            if (args.Length >= 2)
+            {
+                System.Console.WriteLine("Hello World!{0} is {1}", args[0], args[1]);
+            }
+            else{
+                System.Console.WriteLine("Hello World!");
+            }
+        }
+
+    }
+    public class Method
+    {
+        static void Main(String[] args)
+        {
+            Hello hello
+                = new Hello(args);
+
+        }
+    }
+}
